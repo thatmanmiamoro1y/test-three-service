02f4b7e6feb03471ce7875b34f1ea4228bfd8c2f
ThatMan <miamoro1y@163.com>
2019-04-05 01:09:44 +0800

    Initialization

--- src/main/java/com/thatman/testservice/client/TestServiceOneClient.java
+++ src/main/java/com/thatman/testservice/client/TestServiceOneClient.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
-@FeignClient(name = "test-service-one",fallback = TestServiceOneClientFallback.class)
+@FeignClient(name = "service-one",fallback = TestServiceOneClientFallback.class)
 public interface TestServiceOneClient {
 
-    @GetMapping(value ="test-service-one/test/getUser")
+    @GetMapping(value ="service-one/test/getUser")
     User getUser();
 
 }
